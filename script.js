b3f056d2a88b275a996cddab1046684baf60ba6f
Ghadeer Elsharkawy <ghadeer.sharkawy@gmail.com>
2022-07-11 01:46:05 +0200

    show modal on any button click

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+let modalButton = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal.hidden');
+const overlay = document.querySelector('.overlay.hidden');
+const closeBtn = document.querySelector('.close-modal');
+console.log(modalButton);
+
+const modalShow = function () {
+    console.log(this.textContent);
+    overlay.classList.remove("hidden");
+    modal.classList.remove("hidden");
+}
+//close button
+modalButton.forEach((btn) => { btn.addEventListener('click', modalShow) });
+const closeModal = function () {
+    modal.classList.toggle("hidden");
+    overlay.classList.toggle("hidden");
+}
+closeBtn.addEventListener('click', closeModal);
