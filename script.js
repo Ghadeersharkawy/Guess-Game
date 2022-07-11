9a74836a7cd7d360ec0ed4fc03121693ca843ff5
Ghadeer Elsharkawy <ghadeer.sharkawy@gmail.com>
2022-07-12 00:07:47 +0200

    show rolled dice number

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let score0 = document.querySelector('#score--0');
 let score1 = document.querySelector('#score--1');
 let diceImg = document.querySelector('.dice');
 const rollBtn = document.querySelector('.btn.btn--roll');
+const holdBtn = document.querySelector('.btn.btn--hold');
+const newBtn = document.querySelector('.btn.btn--new');
 
 //Set Scores to 0 at start
 score0.textContent = 0;
@@ -17,7 +19,8 @@ const getDiceNumber = function () {
     return roll;
 }
 const rollDice = function () {
-    getDiceNumber();
-    console.log(getDiceNumber());
+    let rolledNum = getDiceNumber();
+    diceImg.classList.remove('hidden');
+    diceImg.src = `dice-${rolledNum}.png`;
 }
 rollBtn.addEventListener('click', rollDice);
