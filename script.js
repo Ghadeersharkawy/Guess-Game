13dbfd1ff32d7025340371cd64a705daf6a76b4a
Ghadeer Elsharkawy <ghadeer.sharkawy@gmail.com>
2022-07-12 02:08:46 +0200

    user switch start

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ let diceImg = document.querySelector('.dice');
 const rollBtn = document.querySelector('.btn.btn--roll');
 const holdBtn = document.querySelector('.btn.btn--hold');
 const newBtn = document.querySelector('.btn.btn--new');
+// let currentPlayer1 = document.querySelector('#current--0');
+// let currentPlayer2 = document.querySelector('#current--1');
+//player
+let activePlayer = 0;
 
+let currentScore = 0;
 //Set Scores to 0 at start
 score0.textContent = 0;
 score1.textContent = 0;
@@ -22,5 +27,20 @@ const rollDice = function () {
     let rolledNum = getDiceNumber();
     diceImg.classList.remove('hidden');
     diceImg.src = `dice-${rolledNum}.png`;
+    if (rolledNum == 1)
+    {
+        currentScore = 0;
+        document.querySelector(`.player.player--${activePlayer}`).classList.remove('player--active');
+        //switch users
+        activePlayer = activePlayer == 0 ? 1 : 0;
+        console.log(document.querySelectorAll(`.player.player--${activePlayer}`));
+        document.querySelector(`.player.player--${activePlayer}`).classList.add('player--active');
+    } else
+    {
+        //add rolled num to current 
+        currentScore += rolledNum;
+        document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
+        console.log(currentScore);
+    }
 }
 rollBtn.addEventListener('click', rollDice);
