b357bf8a19f499c388709ae0546faebfc1f219a7
Ghadeer Elsharkawy <ghadeer.sharkawy@gmail.com>
2022-07-13 22:20:40 +0200

    Hold btn functionality

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 'use strict';
 //select Elements
-let score0 = document.querySelector('#score--0');
-let score1 = document.querySelector('#score--1');
+let score0El = document.querySelector('#score--0');
+let score1El = document.querySelector('#score--1');
 let diceImg = document.querySelector('.dice');
 const rollBtn = document.querySelector('.btn.btn--roll');
 const holdBtn = document.querySelector('.btn.btn--hold');
 const newBtn = document.querySelector('.btn.btn--new');
-// let currentPlayer1 = document.querySelector('#current--0');
-// let currentPlayer2 = document.querySelector('#current--1');
 //player
 let activePlayer = 0;
-
+// const activePlayerCurrentEl = document.querySelector(`#current--${activePlayer}`);
 let currentScore = 0;
+let score0 = 0;
+let score1 = 0;
 //Set Scores to 0 at start
-score0.textContent = 0;
-score1.textContent = 0;
+score0El.textContent = score0;
+score1El.textContent = score1;
 //hide dice image
 diceImg.classList.add('hidden');
-
+//Switch Player
+const switchPlayer = function () {
+    document.querySelector(`.player.player--${activePlayer}`).classList.remove('player--active');
+    activePlayer = activePlayer == 0 ? 1 : 0;
+    console.log(document.querySelectorAll(`.player.player--${activePlayer}`));
+    document.querySelector(`.player.player--${activePlayer}`).classList.add('player--active');
+}
 // Roll Dice
 const getDiceNumber = function () {
     let roll = Math.trunc(Math.random() * 6) + 1;
@@ -32,11 +38,7 @@ const rollDice = function () {
         currentScore = 0;
         document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
         //switch users
-
-        document.querySelector(`.player.player--${activePlayer}`).classList.remove('player--active');
-        activePlayer = activePlayer == 0 ? 1 : 0;
-        console.log(document.querySelectorAll(`.player.player--${activePlayer}`));
-        document.querySelector(`.player.player--${activePlayer}`).classList.add('player--active');
+        switchPlayer();
     } else
     {
         //add rolled num to current 
@@ -46,3 +48,22 @@ const rollDice = function () {
     }
 }
 rollBtn.addEventListener('click', rollDice);
+
+// listen to hold btn click
+const holdScore = function () {
+    console.log('hold', currentScore);
+    console.log('player', activePlayer);
+    if (activePlayer == 0)
+    {
+        score0 += currentScore;
+        score0El.textContent = score0;
+    } else
+    {
+        score1 += currentScore;
+        score1El.textContent = score1;
+    }
+    console.log(score0, score1);
+    switchPlayer();
+
+}
+holdBtn.addEventListener('click', holdScore);
